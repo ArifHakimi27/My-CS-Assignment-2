d8b6ebfc0498c0835052aca0850a75096d95cad5
name <email>
2023-04-10 22:58:23 +0900

    First Commit

---
+++ pages/posts/first-page.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+export default function FirstPage() {
+    return (
+        <div>
+            <h2>Hi Muhammad ArifHakimi! Have a nice day :) May you get 4.5! </h2> 
+            <Link href="/">Back to home</Link> 
+        </div>
+    )
+  }
